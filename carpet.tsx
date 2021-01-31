be15ab7fbdf990a1c29839f80db9b4068effcf0d
Hellbiker <78260782+Hellbiker@users.noreply.github.com>
2021-01-31 19:51:56 +0000

    Deploying to gh-pages from master @ 426cdcaf769a4598a46bcb1ecd7bb351958c06c0 🚀

--- carpet.tsx
+++ carpet.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="carpet" tilewidth="32" tileheight="32" tilecount="64" columns="8">
- <image source="../workadventure-map-forumphp-master/tilesets/carpet-34x34.png" width="256" height="256"/>
+ <image source="carpet-34x34.png" width="256" height="256"/>
  <terraintypes>
   <terrain name="carpeted" tile="26"/>
  </terraintypes>
